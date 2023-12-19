c3c49cb486faea660999ffd58790f95d6488456f
Jonathan Bangert <jonathan@bangert.dk>
2023-12-19 19:11:29 +0100

    Add persistance to database and stuff

--- collaboration/index.js
+++ collaboration/index.js
@@ -1,9 +1,46 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const server_1 = require("@hocuspocus/server");
+import { Hocuspocus } from "@hocuspocus/server";
+import { Database } from "@hocuspocus/extension-database";
+
+// Prisma
+import { PrismaClient } from "@prisma/client";
+export const prisma = new PrismaClient();
+
+// Fix bigint issue 
+BigInt.prototype.toJSON = function () {
+  return this.toString(); // Simply converts bigints to strings
+};
+
 // Configure the server …
-const server = new server_1.Hocuspocus({
-    port: 1234,
+const server = new Hocuspocus({
+  port: 1234,
+  address: "localhost",
+  extensions: [
+    new Database({
+      fetch: async ({ documentName }) => {
+        return new Promise((resolve, reject) => {
+            prisma.documents.findFirst({ where: { name: documentName } }).then((document) => {
+                resolve(new Uint8Array(document.data()));
+            }).catch((err) => {
+                reject(err);
+            })
+        });
+      },
+      store: async ({ documentName, state }) => {
+        return new Promise((resolve, reject) => {
+            prisma.documents.upsert({
+                where: { name: documentName },
+                update: { data: state },
+                create: { name: documentName, data: state },
+            }).then((document) => {
+                resolve();
+            }).catch((err) => {
+                reject(err);
+            });
+        });
+      },
+    }),
+  ],
 });
+
 // … and run it!
 server.listen();
--- general/routes/users.js
+++ general/routes/users.js
@@ -1,9 +1,13 @@
 import express from "express";
 var router = express.Router();
 
+import { prisma } from "../app.js";
+
 /* GET users listing. */
 router.get("/", function (req, res, next) {
-  res.send("respond with a resource");
+  prisma.authorizer_users.findMany().then((data) => {
+    res.json(data);
+  });
 });
 
 export default router;
