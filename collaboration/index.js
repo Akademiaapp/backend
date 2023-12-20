9daede13b65cd9c860f252e080cba04b719663af
Jonathan Bangert <jonathan@bangert.dk>
2023-12-20 10:23:26 +0100

    Add dependencies and token verification middleware

--- collaboration/index.js
+++ collaboration/index.js
@@ -1,11 +1,13 @@
 import { Hocuspocus } from "@hocuspocus/server";
 import { Database } from "@hocuspocus/extension-database";
 
+import verifyToken from "./middleware";
+
 // Prisma
 import { PrismaClient } from "@prisma/client";
 export const prisma = new PrismaClient();
 
-// Fix bigint issue 
+// Fix bigint issue
 BigInt.prototype.toJSON = function () {
   return this.toString(); // Simply converts bigints to strings
 };
@@ -17,31 +19,49 @@ prisma.$connect().then(() => {
 // Configure the server …
 const server = new Hocuspocus({
   port: 8090,
-  address: 'localhost',
+  address: "localhost",
+  onAuthenticate: async (data) => {
+    const { token } = data;
+
+    // Check if token is valid
+    const decodedToken = verifyToken(token);
+    if (!decodedToken) {
+      throw new Error("Unauthorized - Token verification failed");
+    } else {
+      console.log(decodedToken);
+      return decodedToken;
+    }
+  },
   extensions: [
     new Database({
       fetch: async ({ documentName }) => {
         return new Promise((resolve, reject) => {
-          prisma.documents.findFirst({ where: { name: documentName } }).then((document) => {
-            if (!document) {
+          prisma.documents
+            .findFirst({ where: { name: documentName } })
+            .then((document) => {
+              if (!document) {
                 return resolve(null);
-            }
-            resolve(new Uint8Array(document.data));
-          }).catch((err) => {
-            reject(err);
-          })
+              }
+              resolve(new Uint8Array(document.data));
+            })
+            .catch((err) => {
+              reject(err);
+            });
         });
       },
       store: async ({ documentName, state }) => {
         return new Promise((resolve, reject) => {
-            prisma.documents.upsert({
-                where: { name: documentName },
-                update: { data: state },
-                create: { name: documentName, data: state },
-            }).then((document) => {
-                resolve();
-            }).catch((err) => {
-                reject(err);
+          prisma.documents
+            .upsert({
+              where: { name: documentName },
+              update: { data: state },
+              create: { name: documentName, data: state },
+            })
+            .then((document) => {
+              resolve();
+            })
+            .catch((err) => {
+              reject(err);
             });
         });
       },
