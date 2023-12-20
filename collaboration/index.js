8419606547e9430a847f718be42348e300b128dc
Jonathan Bangert <jonathan@bangert.dk>
2023-12-20 11:13:33 +0100

    Add document access control in collaboration module

--- collaboration/index.js
+++ collaboration/index.js
@@ -22,16 +22,44 @@ const server = new Hocuspocus({
   address: "localhost",
   onAuthenticate: async (data) => {
     console.log(data)
-    const { token } = data;
+    const { token, documentName } = data;
 
     // Check if token is valid
     const decodedToken = middleware.verifyToken(token);
     if (!decodedToken) {
       throw new Error("Unauthorized - Token verification failed");
-    } else {
-      console.log(decodedToken);
-      return decodedToken;
+      return;
     }
+    console.log(decodedToken);
+    
+    // Check if user has access to document
+    const document = await prisma.documents.findFirst({
+      where: { name: documentName },
+    });
+
+    console.log(document);
+
+    if (!document) {
+      throw new Error("Unauthorized - Document not found");
+      return;
+    }
+
+    const user = await prisma.users.findFirst({
+      where: { id: decodedToken.sub },
+    });
+
+    if (!user) {
+      throw new Error("Unauthorized - User not found");
+      return;
+    }
+
+    if (document.userId !== user.id) {
+      throw new Error("Unauthorized - User does not have access to document");
+      return;
+    }
+
+    // Return user id
+    return user.id;
   },
   extensions: [
     new Database({
